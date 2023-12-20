f34f4b263a969a6ec4b6f3303581913177ea4885
Christopher Byatt <2064371@csfoy.ca>
2023-12-20 12:25:06 -0500

    avancement projet

--- ProgSpecTp2/src/views/MovieDetailsView.vue
+++ ProgSpecTp2/src/views/MovieDetailsView.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Id: {{ id }}</h1>
   <h2>{{ movie.title }}</h2>
-  <img :src="movie.poster_path ? movie.poster_path : 'https://placehold.co/200x200'" width="200"  style="float: right" />
+  <img :src="movie.poster_path ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : 'https://placehold.co/200x200'" width="200"  style="float: right" />
   <h3>{{ movie.vote_average }}/10</h3>
   <p>Date de sortie: {{ movie.release_date }}</p>
   <p>Synopsis: {{ movie.overview }}</p>
@@ -11,13 +11,13 @@
 
 <script>
 import { getMovie } from '@/services/MovieService.js';
-import movieList from "../components/MovieList.vue";
+// import movieList from "../components/MovieList.vue";
 
 export default {
   computed: {
-    movieList() {
-      return movieList
-    }
+    // movieList() {
+    //   return movieList
+    // }
   },
   data() {
     return {
@@ -36,13 +36,13 @@ export default {
       document.title = 'Fiche de ' + this.movie.title + ' - TP2';
     });
   },
-  methods: {
-    getMoviePosterPath() {
-      return this.movie.poster_path
-        ? `https://image.tmdb.org/t/p/w200/${this.movie.poster_path}`
-        : 'https://placehold.co/200x200';
-    },
-  },
+  // methods: {
+  //   getMoviePosterPath() {
+  //     return this.movie.poster_path
+  //       ? `https://image.tmdb.org/t/p/w200/${this.movie.poster_path}`
+  //       : 'https://placehold.co/200x200';
+  //   },
+  // },
 };
 </script>
 
